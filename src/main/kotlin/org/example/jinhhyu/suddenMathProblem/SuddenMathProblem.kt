4506e6833d3a852f7dc1f1fb824fc431e4110afe
jinhyu2357 <jinhyu2357@gmail.com>
2026-03-01 01:20:18 +0900

    Initial commit

---
+++ src/main/kotlin/org/example/jinhhyu/suddenMathProblem/SuddenMathProblem.kt
@@ -0,0 +1,14 @@
+package org.example.jinhhyu.suddenMathProblem
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class SuddenMathProblem : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
